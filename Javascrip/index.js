a7fc6d7b4f2b738e83aa50827a016c2e512a15be
Yohands123 <128442711+Yohands123@users.noreply.github.com>
2023-11-06 20:01:24 +0300

    first commit

---
+++ Javascrip/index.js
@@ -0,0 +1,10 @@
+let a;
+let b;
+let c;
+a=window.prompt("tell me side A");
+a=Number(a)
+b=window.prompt("tell me side b");
+b=Number(b)
+
+c=Math.sqrt(Math.pow(a,2)+Math.pow(a,2))
+console. log(c)
